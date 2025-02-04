e78491ccf5d0cadf73884a59adcc5009239e9e64
Rob <rgladden447@boyslatin.net>
2025-02-04 19:24:29 +0000

    Reviewed Data Types

--- introPython.py
+++ introPython.py
@@ -1,1 +1,45 @@
 message = 'hello from python' 
+
+# The hashtag symbol is a way to make anything invisible to a computer in python.
+
+# text behind the hashatag is great for writing notess to yourself or to other people.
+
+# Datatypes
+
+# "STRING DATA TYPE"
+# A string is anything that is wrapped in qoutes
+
+# "We create strigns by first clicking the quote button and then typing within the quotes"
+
+"We create strings by first clicking on the keyboard and then typing text within the quotes"
+
+"Strings can contain virtually any ky on your keyboard"
+
+"Strings are used mostly for text based data like mesages, comments, etc."
+
+# Integer Data Type
+# An integer is just another way of saying number
+
+# We dont need to wrap numberes in quotes. We can just write them normally.
+
+12
+123
+3488
+0
+
+# When we have  number with a decimal it is considered a float data type.
+
+12.03
+23.56
+
+4+4
+3/1
+
+# Boolean data Type
+# A data representation of true and false
+
+False
+0 # Represents false
+
+True
+1 # represents true
