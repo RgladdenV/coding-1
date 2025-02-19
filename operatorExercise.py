7fe437c07fff9bb1682179688a04f1e4a30424d2
Rob <rgladden447@boyslatin.net>
2025-02-19 19:19:51 +0000

    Completed lesson on operators

---
+++ operatorExercise.py
@@ -0,0 +1,30 @@
+# 1. Which Python operator would you use if you were developing a shopping application and wanted to get the sum of all the items in a person's shopping cart?
+
+# Arithmatic Operator
+bookbag= 20.00
+pens= 10.00
+notebooks= 10.00
+coffeeMug= 20.00 
+
+total = bookbag + pens + notebooks + coffeeMug
+print(total) 
+
+# 2. Which Operator would you use if you wanted to determine if a user is old enough to purchase a video game
+
+# Comparison Operator
+age = 17
+ageToBuy = 17
+
+print(age >= ageToBuy) 
+
+# 3. Boys Latin has asked you to develop a program to track students who are eligible to atten the spring college tour field trip. 
+# The school would like for the program to check each student's ID and do the following: check if the student has taken the coding 1 programming class, 
+# and the student's grade level. If the student has taken the coding class and is in the 12th grade, they may go on the trip.
+# If the student's ID does not meet this condition they cannot attend the trip.
+
+#Logical Operator
+
+studentGrade = 12
+studentHasTakenCode = True
+
+print(studentGrade == 11 and studentHasTakenCode == True) 
---
+++ operators.py
@@ -0,0 +1,51 @@
+
+
+# In python
+
+
+
+
+# To give a variable a value
+
+name = 'Rob' # The equal sign is the operator
+willItSnow = True
+numberOfSandwiches = 10
+
+# Comparison operators
+# comparisons operators allow us to evaluate data based on several factors
+
+10 > 10 # Is 10 less than 20
+
+20.34 >=0 # is 20.34 greater than or equal to 0
+
+100 == 100 # is 100 the SAME AS 100
+
+100 != 10 # is 100 NOT the SAME as 10
+
+# Arithmatic Operators
+# the arithmic operators allow us to perform math with- specifically with the integer and float data types
+
+10 + 10 # addition
+12-2 # subtraction
+10 * 8 # multiplication
+2/1 # division
+
+# Assignment operators
+# assignment operators use some combination 
+
+# Logical Operators
+# we use logical operators to evaluate if data is true or false
+
+password = '123'
+securityQuestion = 'Franks'
+
+# and - used to compare to conditions that are both true
+
+# if both conditions below are true, you will have the value of true
+password == '123' and 4 == mathAnswer
+# or - used to compare conditions. But only one needs to be true in order for the output to be true.
+
+# not - used to compare conditions BUT will output the opposite result- meaning if two things are true we are going to get false
+
+# whatever the real result is, will become the opposite
+not(password == '123' and 2+2 == mathAnswer)
