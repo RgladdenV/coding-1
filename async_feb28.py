cc39d8fa62f93ea43dc9f8608e3c72313d3082ea
Rob <rgladden447@boyslatin.net>
2025-02-28 19:22:15 +0000

    Completed async work for Feb, 28th

---
+++ async_feb28.py
@@ -0,0 +1,23 @@
+# 1. In your own words describe what a function is.
+
+# A function is something that triggers a command from the computer.
+
+# 2. Create a function that will take in a users data and then print the data out. 
+# Hint: you will need to use the input() function to accomplish this. 
+# Research the input() function on W3schools if you need a refresher on how it works.
+
+password = input
+
+input(TonyStank)
+
+print(password)
+
+# 3. You have been hired to create an affordability checker for a real estate company. 
+# The company would like you to create a function that will take in a users budget, 
+# compare the budget to a $500,000.00 house, and print out if the user has enough money to purchase the home. 
+# If they do have enough money, it should print out true, if they don not, it should print out false.
+
+x = 146,987.00
+y = 500,000.00
+
+print(x>y)
---
+++ functions.py
@@ -0,0 +1,55 @@
+x = 10
+y = 20
+
+print(x<y)
+
+title = 'engineer'
+years = 10
+
+print(title)
+print(years)
+
+# Function examples
+def compare():
+    x = 10
+    y = 20
+    print(x<y)
+
+compare()
+
+def text():
+    title = 'engineer'
+    years = 10
+    print(title)
+    print(years)
+
+text()
+
+def greeting():
+    print("Welcome to python functions.")
+
+greeting()
+
+# My own functions
+def addition():
+    x = 5
+    y = 13
+    z = 57
+    print(x+y+z)
+
+addition()
+
+def compare():
+    x = 3
+    y = 1
+    print(x>y)
+
+compare()
+
+def division():
+    x = 20
+    y = 2
+    print(x/y)
+
+division()
+
