36981ccfc5f4e76c764e143997b986d2cc4dc03d
Rob <rgladden447@boyslatin.net>
2025-03-04 19:18:04 +0000

    Finished function lesson for today

---
+++ Unit 2/functionLesson2.py
@@ -0,0 +1,26 @@
+# create a clone function where data that is passed in will be opied and then printed out
+
+def cloneMachine(name):
+    print('this is the original: '+ name)
+    print('this is the clone: '+ name)
+
+cloneMachine('Rob')
+
+def cloneMachine(number):
+    result= str(number)
+    print('original number is: '+ result)
+    print(2 * number)
+
+cloneMachine(8)
+
+def email(emailAddress, password)
+    captcha = 'aseoep'
+
+    if emailAddress and password:
+        print('great you have access')
+    if captcha == captcha:
+        ('2 step complete')
+    else:
+        print('you cannot enter')
+
+email('Rob','eiofeiwoj')
